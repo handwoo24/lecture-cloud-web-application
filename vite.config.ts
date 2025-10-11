86d8edfd2aff4d202e6bb27bfb7d37913f632cf5
handwoo24 <handwoo24@gmail.com>
2025-10-11 23:48:30 +0900

    :heavy_plus_sign: Nitro 설치하기

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
 
 const config = defineConfig({
   plugins: [
@@ -12,6 +13,8 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
+    // firebase-app-hosting을 위한 preset을 설정합니다.
+    nitroV2Plugin({ preset: 'firebase-app-hosting' }),
     viteReact(),
   ],
 })
