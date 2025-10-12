d34bd6d10e8504787cd7ceb20ae3e74dc056230a
handwoo24 <handwoo24@gmail.com>
2025-10-12 10:19:08 +0900

    :sparkles: 구글 로그인 세션 구현하기

---
+++ src/server/session.ts
@@ -0,0 +1,21 @@
+import { createServerOnlyFn } from '@tanstack/react-start'
+import { useSession } from '@tanstack/react-start/server'
+
+type Session = {
+  token?: string
+  expires?: number
+  uid?: string
+}
+
+export const useAuthSession = createServerOnlyFn(() => {
+  const AUTH_SECRET = process.env.AUTH_SECRET
+
+  if (typeof AUTH_SECRET !== 'string') {
+    throw new Error('Missing AUTH_SECRET')
+  }
+
+  return useSession<Session>({
+    password: AUTH_SECRET,
+    maxAge: 60 * 60,
+  })
+})
