ec0c5cdcb9e06de5c5e8fc77247a4d9513f7157e
handwoo24 <handwoo24@gmail.com>
2025-10-14 13:52:54 +0900

    :sparkles: UI 기능 구현하기

--- src/routes/_navbar/index.tsx
+++ src/routes/_navbar/index.tsx
@@ -1,33 +1,99 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Search } from 'lucide-react'
+import { z } from 'zod'
+import { useCallback } from 'react'
+import type { FormEvent } from 'react'
+
+enum Category {
+  Shoes = 'shoes',
+  Tshirts = 'tshirts',
+}
+
+const zodCategorySchema = z.nativeEnum(Category)
 
 export const Route = createFileRoute('/_navbar/')({
   component: App,
   loader() {},
+  // URL 쿼리 파라미터를 검증합니다.
+  validateSearch: z.object({
+    name: z.string().optional(),
+    category: zodCategorySchema.optional(),
+  }),
 })
 
+const parseFormValue = (form: HTMLFormElement, name: string) => {
+  const formData = new FormData(form)
+  return formData.get(name)
+}
+
 function App() {
+  const search = Route.useSearch()
+  const navigate = Route.useNavigate()
+
+  const updateSearchParams = useCallback(
+    (searchParams: typeof search) =>
+      navigate({ to: '/', search: searchParams }),
+    [navigate],
+  )
+
+  const handleSubmitFilter = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      const formValue = parseFormValue(e.currentTarget, 'category')
+      const category = zodCategorySchema.parse(formValue)
+      updateSearchParams({ ...search, category })
+    },
+    [navigate, search],
+  )
+
+  const handleSubmitSearch = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      const formValue = parseFormValue(e.currentTarget, 'name')
+      const name = z.string().parse(formValue)
+      updateSearchParams({ ...search, name: name === '' ? undefined : name })
+    },
+    [navigate, search],
+  )
+
+  const handleResetFilter = useCallback(() => {
+    updateSearchParams({ ...search, category: undefined })
+  }, [navigate, search])
+
   return (
     <main>
       <div className="product-control-bar">
-        <label className="input w-full md:max-w-sm">
-          <input type="search" placeholder="상품명으로 검색하세요." />
-          <span className="label">
-            <Search />
-          </span>
-        </label>
-        <form className="filter">
+        <form className="w-full md:max-w-sm" onSubmit={handleSubmitSearch}>
+          <label className="input w-full">
+            <input
+              name="name"
+              type="search"
+              defaultValue={search.name}
+              placeholder="상품명으로 검색하세요."
+            />
+            <span className="label">
+              <Search />
+            </span>
+          </label>
+        </form>
+        <form
+          className="filter"
+          onChange={handleSubmitFilter}
+          onReset={handleResetFilter}
+        >
           <input className="btn btn-square" type="reset" value="×" />
           <input
             className="btn"
             type="radio"
-            name="frameworks"
-            aria-label="운동화"
+            name="category"
+            value="shoes"
+            defaultChecked={search.category === Category.Shoes}
+            aria-label="신발"
           />
           <input
             className="btn"
             type="radio"
-            name="frameworks"
+            name="category"
+            value="tshirts"
+            defaultChecked={search.category === Category.Tshirts}
             aria-label="티셔츠"
           />
         </form>
