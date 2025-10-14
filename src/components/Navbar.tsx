d72f29dbc81d8d87e98f8a4219d73b8604789dd2
handwoo24 <handwoo24@gmail.com>
2025-10-14 19:33:03 +0900

    :sparkles: 메시지 적용하기

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import { User } from 'lucide-react'
+import { m } from '@/paraglide/messages'
 
 export const Navbar = () => {
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">My App</a>
+        <a className="btn btn-ghost text-xl">{m.app_title()}</a>
       </div>
       <div className="flex-none">
         <button className="btn btn-square btn-ghost">
