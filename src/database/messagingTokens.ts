68f1c3f3cd9005ace8456d65c362bb10906e1fcb
handwoo24 <handwoo24@gmail.com>
2025-10-22 12:45:41 +0900

    :sparkles: messaginToken 가져오기

--- src/database/messagingTokens.ts
+++ src/database/messagingTokens.ts
@@ -1,6 +1,8 @@
 import { createServerOnlyFn } from '@tanstack/react-start'
 import insertMessagingTokenQuery from './sql/insert_messaging_token.sql?raw'
+import selectMessagingTokensQuery from './sql/select_messaging_tokens.sql?raw'
 import { getPool } from './config'
+import { zodMessagingTokenSchema } from '@/model/messagingToken'
 
 export const createMessagingToken = createServerOnlyFn(
   async (uid: string, fcmToken: string) => {
@@ -12,3 +14,14 @@ export const createMessagingToken = createServerOnlyFn(
     }
   },
 )
+
+export const getMessagingTokens = createServerOnlyFn(async (uid: string) => {
+  try {
+    const pool = getPool()
+    const res = await pool.query(selectMessagingTokensQuery, [uid])
+
+    return zodMessagingTokenSchema.array().parse(res.rows)
+  } catch (error) {
+    throw new Error('Failed to get messaging token: ' + error)
+  }
+})
---
+++ src/database/sql/select_messaging_tokens.sql
@@ -0,0 +1,3 @@
+SELECT *
+FROM messaging_tokens
+WHERE uid = $1;
